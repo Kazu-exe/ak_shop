5a273fc4e12442859d6179819934672d5163515e
Kazu <85038692+Kazu-exe@users.noreply.github.com>
2021-05-30 13:26:15 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -87,9 +87,28 @@ Citizen.CreateThread(function()
     end)
 
     local position = {
-        {x = 25.67 , y = -1346.37, z = 29.49, }
-    }    
-    
+        {x = 25.67 , y = -1346.37, z = 29.49 },
+        {x = -1222.84 , y = -907.0730, z = 12.32 },
+        {x = -1487.64 , y = -379.14, z = 40.16 },
+        {x = -707.41 , y = -914.1264, z = 19.21 },
+        {x = 373.93 , y = 326.17, z = 103.56 },
+        {x = 1163.41 , y = -323.82, z = 69.20 },
+        {x = 374.07 , y = 326.73, z = 103.56 },
+        {x = 2557.458, y = 382.282, z = 108.319},
+        {x = -3038.939, y = 585.954, z = 7.908},
+        {x = -3241.927, y = 1001.462, z = 12.830},
+        {x = 547.431, y = 2671.710, z = 42.156},
+        {x = 1961.464, y = 3740.672, z = 32.343},
+        {x = 2678.916, y = 3280.671, z = 55.241},
+        {x = 1729.216, y = 6414.131, z = 35.037},
+        {x = 1135.808, y = -982.281, z = 46.415},
+        {x = -2968.243, y = 390.910, z = 15.043},
+        {x = 1166.024, y = 2708.930, z = 38.157},
+        {x = 1392.562, y = 3604.648, z = 34.980},
+        {x = -48.519, y = -1757.514, z = 29.421},
+        {x = -1820.523, y = 792.518, z = 138.118},
+        {x = 1698.388, y = 4924.404, z = 42.063}
+    }
     
     Citizen.CreateThread(function()
         while true do
@@ -102,13 +121,10 @@ Citizen.CreateThread(function()
     
                 if dist <= 1.0 then
 
-                   RageUI.Text({
-                        message = "Appuyez sur [~b~E~w~] pour parler a ~b~Apu",
-                        time_display = 1
-                    })
-                   -- ESX.ShowHelpNotification("Appuyez sur [~b~E~w~] pour acceder au ~b~Shop")
+                   ESX.ShowHelpNotification("Appuyez sur [~b~E~w~] pour parler a~b~Apu")
+
                     if IsControlJustPressed(1,51) then
-                        RageUI.Visible(RMenu:Get('shop', 'main'), not RageUI.Visible(RMenu:Get('shop', 'main')))
+                        RageUI.Visible(RMenu:Get('Modeo', 'main'), not RageUI.Visible(RMenu:Get('Modeo', 'main')))
                     end
                 end
             end
